62cac02468a96a337a851af716daa4f168f46ef5
ecareyu <ecareyu@gmail.com>
2018-02-25 18:37:13 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'brook-gui-multi-client'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
